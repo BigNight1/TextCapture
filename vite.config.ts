bcd468bc63ffd0c21d0fb1a7cb4f794f972a6b73
Edu Armas <edu_armas11@hotmail.com>
2025-10-13 19:02:16 -0500

    mmmm

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    port: 5176,
-    allowedHosts: [
-      'localhost',
-      '.ngrok.io',
-      '.ngrok-free.app',
-      '.ngrok.app'
-    ],
-    cors: true,
-    headers: {
-      'Cross-Origin-Embedder-Policy': 'unsafe-none',
-      'Cross-Origin-Opener-Policy': 'unsafe-none'
-    }
-  }
+  
 })
